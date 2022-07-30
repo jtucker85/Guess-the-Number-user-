9155baf3b9d06786bac82ae7fe9298b5271b586d
James Tucker <j.tucker07@gmail.com>
2022-07-30 20:27:56 +0100

    added guess counter

--- guessthenumber.py
+++ guessthenumber.py
@@ -1,17 +1,21 @@
 import random
 
 gr = False
-
+count = 1
 b = 1
 t = 1000
 
+print("Think of a number between 1 and 1000! I'm going to amaze you by guessing it.")
+
 while gr == False:
     num = random.randint(b, t)
-    ans = input(f"Is {num} too high (h), too low (l) or correct (c)?")
+    ans = input(f"I guess {num}! Is it too high (h), too low (l) or correct (c)?")
     if ans == "h":
         t = num - 1
+        count = count +1
     elif ans =="l":
         b = num + 1
+        count = count +1
     else:
-        print(f"Wow! I am so awesome, right? You number was {num}.")
+        print(f"Wow! I am so awesome, right? You number was {num}. And it only took me {count} guesses.")
         gr = True
